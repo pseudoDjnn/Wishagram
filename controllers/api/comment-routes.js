c70be2b1359d8ea9b314e386bcd5dc6939eefe5c
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-22 18:48:29 -0400

    add api universal gets

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,7 +1,32 @@
+const { Post } = require("../../models");
+
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.render("Response");
+// router.get("/", (req, res) => {
+//   res.render("Response");
+// });
+
+// GRAB ALL COMMENTS
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["id", "username"],
+        },
+        {
+          model: Post,
+          attributes: ["id", "title"],
+        },
+      ],
+    });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    res.json(comments);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
