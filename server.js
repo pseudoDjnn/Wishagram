b507f1abe89935755d1a198e8f3ea7d8561827de
p5eudoDjnn <alexander.havers@gmail.com>
2022-10-31 21:27:19 -0400

    working on heroku fix

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.set("view engine", "handlebars");
 app.use(require("./controllers/"));
 
 //turn on connectection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () =>
     console.log(`App listening on http://localhost:${PORT}`)
   );
